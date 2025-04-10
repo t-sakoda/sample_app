454be5437202a6e558ff50eac2ae7c9e24416ef8
Sakoda, Taro <sakoda.taro.229@gmail.com>
2025-04-10 21:13:06 +0900

    ♻️ Update error handling in SessionsController to set flash message for invalid login attempts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報ページにリダイレクトする
     else
-      # エラーメッセージを作成する
+      flash[:danger] = 'Invalid email/password combination'
       render 'new', status: :unprocessable_entity
     end
   end
