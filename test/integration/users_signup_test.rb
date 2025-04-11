c5ee6da80744b432af202065d2178e869eb9d889
Sakoda, Taro <sakoda.taro.229@gmail.com>
2025-04-11 14:32:27 +0900

    ✨ Implement user login after successful signup and add login check in tests

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -26,5 +26,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template "users/show"
     assert_not flash.empty?
     assert_select "div.alert.alert-success", "Welcome to the Sample App!"
+    assert is_logged_in?
   end
 end
