2159175a7089cc67f3ddf8b82bdd049b78d4cf4b
Sakoda, Taro <sakoda.taro.229@gmail.com>
2025-04-16 19:06:53 +0900

    ✨ Add image variant processing with MiniMagick and update image display logic in microposts

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module SampleApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # ImageMagick
+    config.active_storage.variant_processor = :mini_magick
   end
 end
