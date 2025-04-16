bf42afe71dd7c807967074b93be113b5b7eb725e
Sakoda, Taro <sakoda.taro.229@gmail.com>
2025-04-16 22:35:13 +0900

    🔧 Configure Active Storage to use Amazon S3 for file uploads and update storage.yml with environment variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,6 @@ Rails.application.configure do
     :authentication => :plain,
   }
 
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
@@ -106,4 +105,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # アップロードされたファイルをAWSに保存する
+  config.active_storage.service = :amazon
 end
