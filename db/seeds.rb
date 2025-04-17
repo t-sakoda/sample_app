6bf7ae039e47d24ab48f10bf473884f526de7965
Sakoda, Taro <sakoda.taro.229@gmail.com>
2025-04-17 10:56:29 +0900

    🗃️ フォロワーとフォローのリレーションシップを作成するためのユーザーシードデータを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,18 @@
 # メインのサンプルユーザーを1人作成する
-User.create!(name: "Example User",
+User.create!(name:  "Example User",
              email: "example@railstutorial.org",
-             password: "foobar1234567",
+             password:              "foobar1234567",
              password_confirmation: "foobar1234567",
-             admin: true,
+             admin:     true,
              activated: true,
              activated_at: Time.zone.now)
 
 # 追加のユーザーをまとめて生成する
 99.times do |n|
-  name = Faker::Name.name
+  name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password1234567"
-  User.create!(name: name,
+  User.create!(name:  name,
                email: email,
                password:              password,
                password_confirmation: password,
@@ -26,3 +26,11 @@ users = User.order(:created_at).take(6)
   content = Faker::Lorem.sentence(word_count: 5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# ユーザーフォローのリレーションシップを作成する
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
